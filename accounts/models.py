7ba1513611e34d451fc25455bb9227eecd953818
sreebash <sreebashdas.1@gmail.com>
2022-06-29 13:32:22 +0600

    feat: add jwt authentication module for multiple users(admin, vendors, customers)

---
+++ accounts/models.py
@@ -0,0 +1,46 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # These fields tie to the roles!
+    ADMIN = 1
+    VENDOR = 2
+    CUSTOMER = 3
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (VENDOR, 'Vendor'),
+        (CUSTOMER, 'Customer')
+    )
+
+    uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=3)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.EmailField()
+    modified_by = models.EmailField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
